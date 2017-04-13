91ccfd8a127be2e23f2d5b9954de85e169297fd0
Mauricio Farinelli <Mauricio Farinelli>
2017-04-12 22:02:52 -0400

    Add-Shift lazy loaded without route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, SystemJsNgModuleLoader, ModuleWithProviders } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgReduxModule } from '@angular-redux/store';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainComponent  } from './main/main.component';
 import { ToolbarButtonComponent } from './toolbar-button/toolbar-button.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
-import {ToolbarService } from './toolbar.service';
-import { AddShiftComponent } from './add-shift/add-shift.component';
+import { ToolbarService } from './toolbar.service';
+
+import { AdDirective } from './ad.directive';
 import { AddPaycodeComponent } from './add-paycode/add-paycode.component';
 import { ReplaceShiftComponent } from './replace-shift/replace-shift.component';
 import { SliderComponent } from './slider/slider.component';
 
+const routes: Routes = [
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'main', component: MainComponent },
+  {path: 'add-shift', loadChildren: './add-shift/add-shift.module#AddShiftModule'}
+];
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
+
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
     ToolbarButtonComponent,
     ToolbarComponent,
-    AddShiftComponent,
     AddPaycodeComponent,
     ReplaceShiftComponent,
-    SliderComponent
+    SliderComponent,
+    AdDirective
   ],
+  //entryComponents: [AddShiftComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     NgReduxModule,
+    routing
   ],
-  providers: [ToolbarService],
+  providers: [ToolbarService, SystemJsNgModuleLoader],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SystemJsNgModuleLoader, NgModuleFactory, ViewChild, ViewContainerRef, ReflectiveInjector } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { AppState } from '../store/store';
+
+import { AdDirective } from '../ad.directive';
+import { AddShiftComponent } from '../add-shift/add-shift.component';
 
 @Component({
   selector: 'slider',
@@ -6,8 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
-  constructor() {}
+  private actionSelected;
+  private action;
+  private componentRef;
 
-  ngOnInit() {}
+  @ViewChild(AdDirective) adHost: AdDirective;
+
+  constructor(
+    private ngRedux: NgRedux<AppState>,
+    private loader: SystemJsNgModuleLoader,
+    public viewContainerRef: ViewContainerRef
+  ) {}
+
+  ngOnInit() {
+    this.actionSelected = this.ngRedux.select(state => state.context)
+      .subscribe(context => {
+        this.action = context.actionId;
+
+        this.loader.load('./add-shift/add-shift.module#AddShiftModule').then((factory: NgModuleFactory<any>) => {
+          let injector = ReflectiveInjector.fromResolvedProviders([], this.viewContainerRef.parentInjector);
+          let factoryInjector = factory.create(injector);
+          this.loadComponent(factoryInjector, AddShiftComponent);
+        });
+      });
+  }
+
+  loadComponent(factoryInjector, component) {
+    let componentFactory = factoryInjector.resolveComponentFactory(component);
+
+    let viewContainerRef = this.adHost.viewContainerRef;
+    viewContainerRef.clear();
+    this.componentRef = viewContainerRef.createComponent(componentFactory);
+  }
+
+
 
 }
